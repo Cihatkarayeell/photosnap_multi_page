930dbdc20ad54217c7cd9455737b50a0b88863b6
Cihat <cihatkarayel@icloud.com>
2024-08-10 10:49:50 +0300

    bug fixed

--- src/js/partial/init.js
+++ src/js/partial/init.js
@@ -1,4 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const mobileMenuOpened = document.querySelector('.mobile-toggle');
+    const mobileMenu = document.querySelector('.mobile-menu');
+    const mobileMenuOpen = document.querySelector('.open-toggle');
+    const mobileMenuClose = document.querySelector('.close-toggle');
+    const main = document.querySelector('.main');
+    const header = document.querySelector('.header');
 
+    if (mobileMenuOpened && mobileMenu && mobileMenuOpen && mobileMenuClose && main && header) {
+        mobileMenuOpened.addEventListener('click', () => {
+            mobileMenu.classList.toggle('active');
+            mobileMenuClose.classList.toggle('active');
+            mobileMenuOpen.classList.toggle('active');
+            main.classList.toggle('active');
+            header.classList.toggle('active');
+        });
+    } else {
+        console.error('One or more elements could not be found.');
+    }
+});
 
 const toggleBall = document.querySelector('.toggle-section');
 const pricingCards = document.querySelectorAll('.pricing-card');
@@ -9,19 +28,3 @@ toggleBall.addEventListener('click', () => {
         card.classList.toggle('yearly');
       });
 }) 
-
-
-var mobileMenuOpened = document.querySelector('.mobile-toggle');
-var mobileMenu = document.querySelector('.mobile-menu');
-const mobileMenuOpen = document.querySelector('.open-toggle');
-const mobileMenuClose = document.querySelector('.close-toggle');
-const main = document.querySelector('.main');
-const header = document.querySelector('#header');
-
-mobileMenuOpened.addEventListener('click', () => {
-    mobileMenu.classList.toggle('active');
-    mobileMenuClose.classList.toggle('active');
-    mobileMenuOpen.classList.toggle('active');
-    main.classList.toggle('active');
-    header.classList.toggle('active');
-});
